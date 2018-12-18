1a1bb2a9cd03cb1006cdaa56ed62c8d80e8d8f5b
Wei-Chih, Huang <noctildon2@gmail.com>
2018-12-18 17:11:25 +0800

    dev in new func

--- call_lin_fit.py
+++ call_lin_fit.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QFileDialog
-from ui_lin_fit import Ui_MainWindow  # import UI
+from ui_lin_fit import Ui_MainWindow
 import pyqtgraph as pg
 import numpy as np
 from scipy import optimize
@@ -12,26 +12,36 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(MyMainWindow, self).__init__(parent)
 
-        # figure option setting. NOTE: this needs to be above self.setupUi(self)
-        pg.setConfigOption('background', '#f0f0f0')  # set background color
-        pg.setConfigOption('foreground', 'd')        # set foreground color
-        pg.setConfigOptions(antialias=True)          # make the graph smooth
+        # setting of figure. NOTE: this needs to be above self.setupUi(self)
+        pg.setConfigOption('background', '#f0f0f0')
+        pg.setConfigOption('foreground', 'd')
+        pg.setConfigOptions(antialias=True)
 
         self.setupUi(self)
         self.btn_clean.clicked.connect(self.clean_all)
-        self.btn_load.clicked.connect(self.getfile)
-        self.show_eq.setReadOnly(True)  # set show_eq to be read only
+        # self.btn_load.clicked.connect(self.getfile)
+        self.btn_load.clicked.connect(self.getequ)  # testing function
+        self.show_eq.setReadOnly(True)
+
+    def getequ(self):
+        str = self.fit_eq.toPlainText().split("\n")
+        args = str[0].replace(" ", "").split(',')
+
+        func_args = ""
+        for arg in args
+            func_args = "fit_func = lambda x"
+        # to_be_exec = "global to_fit_equ;" + "to_fit_equ=" + equ_str
+        # exec(to_be_exec)
 
     @pyqtSlot()
     def getfile(self):
-        # filter file and get the file path
         file_path, _ = QFileDialog.getOpenFileName(
             self, 'Open file', './', "(*.csv *.ods *.xlsm *.xlsx)")
-        if file_path:  # check if the file is loaded
+        if file_path:
             data_read = get_data(file_path)
             try:
-                x_data = list(data_read.values())[0][0]  # first line of data
-                y_data = list(data_read.values())[0][1]  # second line of data
+                x_data = list(data_read.values())[0][0]
+                y_data = list(data_read.values())[0][1]
                 self.renew(x_data, y_data)
             except ValueError:
                 QMessageBox.information(self, "ValueError", "Some elements in data are not float.")
@@ -40,37 +50,34 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
 
     # plot the data and fitting curve to the window
     def renew(self, x_data, y_data):  # x_data and y_data must be lists
-        # change the list to numpy list
         x_data = np.asarray(x_data)
         y_data = np.asarray(y_data)
 
-        self.clean_all()  # clean the window and the equation
+        self.clean_all()
         plt = self.show_win.addPlot()  # ready to plot
-        # plot the raw data point
         plt.plot(x_data, y_data, pen=None, symbol='o')
 
         def fit_func(x, a, b):  # define optimize function
             return a * x + b
 
-        params, params_covariance = optimize.curve_fit(fit_func, x_data, y_data)  # fit the raw data
-        a_fit = params[0]  # fitting curve a parameter
-        b_fit = params[1]  # fitting curve b parameter
+        params, params_covariance = optimize.curve_fit(fit_func, x_data, y_data)
+        a_fit = params[0]
+        b_fit = params[1]
         plt.plot(x_data, fit_func(x_data, a_fit, b_fit),
-                 pen=pg.mkPen(color='#333', width=2))  # plot the fitting curve
+                 pen=pg.mkPen(color='#333', width=2))
         equ = "y = " + "%.3e" % (a_fit) + " x + " + "%.3e" % (b_fit)
 
         # standard deviation errors, first one is for x, second one is for y
         SDEs = np.sqrt(np.diag(params_covariance))
-        x_sd = SDEs[0]  # standard deviation errors of x
-        y_sd = SDEs[1]  # standard deviation errors of y
-        # show the fitting curve equation
+        x_sd = SDEs[0]
+        y_sd = SDEs[1]
         self.show_eq.setText(equ + "\n" + "standard deviation of x: " + "%.3e" % (x_sd) +
                              "\n" + "standard deviation of y: " + "%.3e" % (y_sd))
 
     @pyqtSlot()
     def clean_all(self):
-        self.show_win.clear()     # clean the window
-        self.show_eq.setText("")  # clean the equation
+        self.show_win.clear()
+        self.show_eq.setText("")
 
 
 if __name__ == "__main__":
