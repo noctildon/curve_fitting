09f8f34f5930c8e73ec7d8ec237a1820898ad565
Wei-Chih, Huang <noctildon2@gmail.com>
2018-12-19 17:24:31 +0800

    add feature to fit customized function

--- call_lin_fit.py
+++ call_lin_fit.py
@@ -3,7 +3,7 @@ from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QFileDialog
 from ui_lin_fit import Ui_MainWindow
 import pyqtgraph as pg
-import numpy as np
+from numpy import *
 from scipy import optimize
 from pyexcel_ods import get_data
 
@@ -19,60 +19,64 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
 
         self.setupUi(self)
         self.btn_clean.clicked.connect(self.clean_all)
-        # self.btn_load.clicked.connect(self.getfile)
-        self.btn_load.clicked.connect(self.getequ)  # testing function
+        self.btn_load.clicked.connect(self.getfile)
+        # self.btn_load.clicked.connect(self.getfunc)
         self.show_eq.setReadOnly(True)
 
-    def getequ(self):
+    def getfunc(self):
+        # first line is arguments, second line is the function to be fitted
         str = self.fit_eq.toPlainText().split("\n")
         args = str[0].replace(" ", "").split(',')
-
-        func_args = ""
-        for arg in args
-            func_args = "fit_func = lambda x"
-        # to_be_exec = "global to_fit_equ;" + "to_fit_equ=" + equ_str
-        # exec(to_be_exec)
+        to_be_exec = "fit_func = lambda x"
+        for arg in args:
+            to_be_exec += ", " + arg
+        to_be_exec += ":" + str[1]
+        exec("global fit_func;" + to_be_exec)
+        return args
 
     @pyqtSlot()
     def getfile(self):
-        file_path, _ = QFileDialog.getOpenFileName(
-            self, 'Open file', './', "(*.csv *.ods *.xlsm *.xlsx)")
-        if file_path:
-            data_read = get_data(file_path)
-            try:
-                x_data = list(data_read.values())[0][0]
-                y_data = list(data_read.values())[0][1]
-                self.renew(x_data, y_data)
-            except ValueError:
-                QMessageBox.information(self, "ValueError", "Some elements in data are not float.")
-            except RuntimeError:
-                QMessageBox.information(self, "RuntimeError", "The least-squares minimization fails")
+        if self.fit_eq.toPlainText() == "":
+            QMessageBox.information(self, "Empty value", "Empty equation format\nInsert the function to be fitted")
+        else:
+            self.getfunc()
+            file_path, _ = QFileDialog.getOpenFileName(
+                self, 'Open file', './', "(*.csv *.ods *.xlsm *.xlsx)")
+            if file_path:
+                data_read = get_data(file_path)
+                try:
+                    x_data = list(data_read.values())[0][0]
+                    y_data = list(data_read.values())[0][1]
+                    self.renew(fit_func, x_data, y_data)
+                except ValueError:
+                    QMessageBox.information(self, "Value Error", "Some elements in data are not float.")
+                except RuntimeError:
+                    QMessageBox.information(self, "Runtime Error", "The least-squares minimization fails")
 
     # plot the data and fitting curve to the window
-    def renew(self, x_data, y_data):  # x_data and y_data must be lists
-        x_data = np.asarray(x_data)
-        y_data = np.asarray(y_data)
+    def renew(self, fit_func, x_data, y_data):  # x_data and y_data must be lists
+        def wrapper(func, args):
+            return func(*args)
 
         self.clean_all()
         plt = self.show_win.addPlot()  # ready to plot
         plt.plot(x_data, y_data, pen=None, symbol='o')
 
-        def fit_func(x, a, b):  # define optimize function
-            return a * x + b
-
         params, params_covariance = optimize.curve_fit(fit_func, x_data, y_data)
-        a_fit = params[0]
-        b_fit = params[1]
-        plt.plot(x_data, fit_func(x_data, a_fit, b_fit),
-                 pen=pg.mkPen(color='#333', width=2))
-        equ = "y = " + "%.3e" % (a_fit) + " x + " + "%.3e" % (b_fit)
+        params = params.tolist()
 
-        # standard deviation errors, first one is for x, second one is for y
-        SDEs = np.sqrt(np.diag(params_covariance))
-        x_sd = SDEs[0]
-        y_sd = SDEs[1]
-        self.show_eq.setText(equ + "\n" + "standard deviation of x: " + "%.3e" % (x_sd) +
-                             "\n" + "standard deviation of y: " + "%.3e" % (y_sd))
+        fit_y_data = []
+        for x in x_data:
+            fit_y_data.append(wrapper(fit_func, [x] + params))
+
+        plt.plot(x_data, fit_y_data, pen=pg.mkPen(color='#333', width=2))
+
+        args = self.getfunc()
+        SDEs = sqrt(diag(params_covariance))  # standard deviation errors
+        show_text = ''
+        for id, arg in enumerate(args):
+            show_text += '{} = {:.2e} ± {:.2e} \n'.format(arg, params[id], SDEs[id])
+        self.show_eq.setText(show_text)
 
     @pyqtSlot()
     def clean_all(self):
